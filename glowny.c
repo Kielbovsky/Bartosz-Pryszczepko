e6a41f01311d5f2c570d10e73a148d5ea64db482
Bartosz Pryszczepko <bpryszczepko@linux-p110-2.inf.ug.edu.pl>
2017-10-22 14:14:23 +0200

    tak

---
+++ glowny.c
@@ -0,0 +1,5 @@
+#include "plik.h"
+int main( ) {
+  hej();
+  return 0;
+}
---
+++ zad10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int x;
+  x = getchar();
+  putchar(x);
+
+  return 0;
+}
